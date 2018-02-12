9c0e4427c1f92826978cbd63d4476402c42fec2d
Christian Nagel <christian@christiannagel.com>
2018-02-12 14:59:11 +0100

    BooksSample v1

---
+++ BooksSample/BooksSample/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksSample.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Publisher { get; set; }
+        public string Isbn { get; set; }
+
+        public override string ToString() => Title;
+    }
+}
